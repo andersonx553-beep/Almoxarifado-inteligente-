89a8dcf9eb2637b16c7dd8467da30b404f2f491e
andersonx553-beep <andersonx553@gmail.com>
2026-09-11 08:42:57 -0300

    ALTERAÇÃO 02 - Atualizar cache PWA para a correção do leitor PDF

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='almoxarifado-v2';
+const CACHE='almoxarifado-v3';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
